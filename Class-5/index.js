2cdfbce68940eea1d238f3ea6c020439352d6be4
GauravBhardwaj <gaurav.bhardwaj@waveaxis.com>
2022-07-12 14:18:46 +0530

    Code Updated

---
+++ Class-5/index.js
@@ -0,0 +1,107 @@
+//-------HTML DOM getElementById() Method--------//
+
+// function callFun() {
+//   document.getElementById("heading").style.backgroundColor = "red";
+// }
+
+//-------HTML DOM getElementsByClassName() Method--------//
+
+// function callFun() {
+//   let ele = document.getElementsByClassName("division");
+
+//   for (let li of ele) {
+//     console.log(li);
+//     li.style.backgroundColor = "yellow";
+//   }
+// }
+
+//------HTML DOM getElementsByTagName () Method-------//
+
+// function callFun() {
+//   let ele = document.getElementsByTagName("li");
+
+//   for (let item of ele) {
+//     item.style.color = "red";
+//     item.style.fontSize = "35px";
+//   }
+// }
+
+//--------HTML DOM getElementsByName () Method----------//
+
+// function callFun() {
+//   let ele1 = document.getElementsByName("fname");
+//   let ele2 = document.getElementsByName("salary");
+
+//   ele1[0].style.backgroundColor = "red";
+//   ele2[0].style.backgroundColor = "blue";
+// }
+
+//--------HTML DOM querySelector () Method----------//
+
+// function callFun() {
+
+// querySelector () Method [ID Element]
+
+//   let ele = document.querySelector("#one");
+//   console.log(ele);
+//   ele.style.fontStyle = "italic";
+//   ele.style.color = "red";
+// }
+
+//--------HTML DOM querySelector () Method----------//
+
+// function callFun() {
+//   querySelector () Method [CLASS Element]
+
+//   let ele = document.querySelector(".headclass");
+//   console.log(ele);
+//   ele.style.fontStyle = "italic";
+//   ele.style.color = "blue";
+// }
+
+//--------HTML DOM querySelectorAll () Method----------//
+
+// function callFun() {
+//   let ele = document.querySelectorAll(".headclass");
+
+//   for (let item of ele) {
+//     item.style.color = "red";
+//   }
+// }
+
+// function callFun() {
+//   let ele = document.querySelectorAll("li");
+
+//   for (let item of ele) {
+//     item.style.color = "red";
+//   }
+// }
+
+//--------HTML DOM innerHTML Property----------//
+
+// function callFun() {
+//   let ele = document.getElementById("head1");
+//   console.log(ele);
+//   ele.innerHTML = "I Am Changed !!!";
+// }
+
+// function callFun() {
+//   let ele1 = document.getElementById("head1");
+//   console.log(ele1);
+//   let ele2 = document.getElementById("head2");
+//   console.log(ele2);
+//   ele2.innerHTML = ele1.innerHTML;
+// }
+
+// function callFun() {
+//   let ele = document.getElementById("outer");
+//   console.log(ele);
+// }
+
+//-----------HTML DOM innerText Property-------------//
+
+// function callFun() {
+//   let ele = document.getElementById("outer").innerText;
+
+//   console.log(ele);
+// }
---
+++ Class-6/index.js
@@ -0,0 +1,156 @@
+/* 
+HTML DOM innerHTML Property 
+&& 
+HTML DOM innerText Property
+*/
+
+// function callFun() {
+//   let ele1 = document.getElementById("head1").innerHTML;
+//   console.log(ele1);
+// }
+
+// function callFun() {
+//   let ele2 = document.getElementById("head1").innerText;
+//   console.log(ele2);
+// }
+
+// function callFun() {
+//   let ele = (document.getElementById("head1").innerHTML = "I Am Changed !!!");
+//   console.log(ele);
+// }
+
+/* --------- HTML DOM textContent Property --------- */
+
+// function callFun() {
+//   let ele = (document.getElementById("head1").textContent = "I Am Changed !!!");
+//   console.log(ele);
+// }
+
+// function callFun() {
+//   let ele = (document.getElementById("head1").childNodes[0].textContent =
+//     "I Am Gaurav Bhardwaj.");
+//   console.log(ele);
+// }
+
+/* ---------outerHTML in javascript--------- */
+
+// function callFun() {
+//   var ele = document.getElementById("division").outerHTML;
+//   console.log(ele);
+// }
+
+// function callFun() {
+//   var ele = (document.getElementById("division").outerHTML =
+//     "I AM CHANGED !!!");
+//   console.log(ele);
+// }
+
+/* ---------outerText in javascript--------- */
+
+// function callFun() {
+//   var ele = (document.getElementById("division").outerText = "I am Changed!");
+//   console.log(ele);
+// }
+
+//---------------- ARRAY ------------------//
+
+// function callFun() {
+//   var numList = [12, 34, 53, 2, 4, 53, 23];
+
+//   var list = numList;
+//   console.log(list);
+// }
+
+//-----------console.warn(message) && console.error( message )----------//
+
+// function callFun() {
+//   console.log("this is console log");
+//   console.warn("this is console warn");
+//   console.error("this is console error");
+// }
+
+//---------console.clear()------------//
+
+// function clearConsole() {
+//   console.clear();
+// }
+
+//-------------JavaScript Debugging---------------//
+
+// function callFun() {
+//   debugger;
+//   var num1 = 10;
+//   var num2 = 20;
+//   debugger;
+//   var sum = num1 + num2;
+//   debugger;
+//   console.log(sum);
+// }
+
+//------------JavaScript Variables--------------//
+
+// var fname = "Gaurav";
+// console.log(fname);
+// var lname = "Bhardwaj";
+// console.log(lname);
+// var fullName = fname + " " + lname;
+// console.log(fullName);
+
+// var list = [344, 5, 3, 5, 35, 6];
+
+// var $numList = list;
+// var _numList = list;
+
+// var fName = "xyz";
+// console.log(fName);
+
+// var let = "abc";
+// console.log(let);
+
+// var fullName = "Gaurav Bhardwaj";
+// let mblno = 5475785785;
+// const city = "Bangalore";
+
+// console.log(fullName);
+// console.log(mblno);
+// console.log(city);
+
+//----------------VAR Keywords----------------//
+
+// var city = "pune";
+// var city = "mumbai";
+
+// console.log(city);
+
+// var mblno = undefined;
+// var city = undefined;
+
+// console.log(mblno);
+
+// var mblno = 4376346743;
+// var city = "pune";
+
+//-----------------Let keywords---------------//
+
+// let city = "pune";
+// let city = "mumbai";
+// console.log(city);
+
+// console.log(city);
+// let city = "pune";
+
+// let city = pune;
+// city = "abc";
+
+// let city = "abc";
+
+// let mblno = 34673734;
+// mblno = "abc";
+// console.log(typeof mblno);
+
+// var x;
+// let y;
+// const z;
+// const city = "pune";
+
+// city = "abc";
